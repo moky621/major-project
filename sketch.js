c7ba2d065174a14d9d616e9bf415e05c98196d1f
moky621 <114016129+moky621@users.noreply.github.com>
2022-11-22 12:16:56 -0600

    works now

--- sketch.js
+++ sketch.js
@@ -1,14 +1,28 @@
 let ball, floor;
 
+let ufo;
 function setup() {
-  createCanvas(80, 160);
+  createCanvas(windowWidth, windowHeight);
   world.gravity.y = 10;
 	
-  ball = new Sprite(40, 30, 50);
+  ball = new Sprite(50, 30, 50);
+  ball.img = ufo;
 
-  floor = new Sprite(40, 155, 80, 5, "static");
+  rocket = new Sprite();
+
+  floor = new Sprite(width/2, height, width, 5, 'static');
+
+  ufo = loadImage("ufo.png");
 }
 
 function draw() {
   clear();
+  if (kb.presses('w')) {
+    //    (direction, speed, distance)
+    ball.move('up', 5, 50);
+  }
+}
+
+function mousePressed() {
+  ball.y -= 50;
 }
