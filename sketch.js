a040946a8c925c3b847fd5a37325be075fe27b14
moky621 <114016129+moky621@users.noreply.github.com>
2022-11-23 12:17:26 -0600

    added ufo image working

--- sketch.js
+++ sketch.js
@@ -1,28 +1,40 @@
-let ball, floor;
-
+let floor;
+let ufoX, ufoY;
 let ufo;
+let ball;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   world.gravity.y = 10;
-	
-  ball = new Sprite(50, 30, 50);
-  ball.img = ufo;
+  ufo = new Sprite(width/2, 0, 10, 80);
+  ufo.addImage("ufo.png");
+  ball = new Sprite(50, 50, 50);
 
-  rocket = new Sprite();
 
-  floor = new Sprite(width/2, height, width, 5, 'static');
+
+  floor = new Sprite(width/2, height-100, width, 5, "static");
 
   ufo = loadImage("ufo.png");
 }
+
 
 function draw() {
   clear();
   if (kb.presses('w')) {
     //    (direction, speed, distance)
-    ball.move('up', 5, 50);
+    ball.move('up', 3, 50);
   }
+
+  
+}
+
+function createUFO(x, y) {
+  ufo = new Sprite(x, y);
+  ufo.addImage(ufo);
+  
+  image(ufo, x, y);
 }
 
 function mousePressed() {
-  ball.y -= 50;
+  ufo.y += 100;
 }
