5762a00412f52bcbf32967772c632dcc50cd007e
moky621 <114016129+moky621@users.noreply.github.com>
2022-11-24 11:12:20 -0600

    added walls and working on collision

--- sketch.js
+++ sketch.js
@@ -2,11 +2,12 @@ let floor;
 let ufoX, ufoY;
 let ufo;
 let ball;
+let wall1;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   world.gravity.y = 10;
-  ufo = new Sprite(width/2, 0, 10, 80);
+  ufo = new Sprite(width/2, 0, 175, 85);
   ufo.addImage("ufo.png");
   ball = new Sprite(50, 50, 50);
 
@@ -20,12 +21,39 @@ function setup() {
 
 function draw() {
   clear();
-  if (kb.presses('w')) {
-    //    (direction, speed, distance)
-    ball.move('up', 3, 50);
+  ufoMove();
+  createWall();
+  checkCollide();
+  
+}
+
+
+function ufoMove(){
+  if (kb.pressing('w')) {
+    ball.vel.y = -3;
   }
 
+  if (kb.pressing('d')) {
+    ball.vel.x = 3;
+  }
   
+  if (kb.pressing('a')) {
+    ball.vel.x = -3;
+  }
+}
+
+function createWall(){
+  wall1 = new Sprite(150, 400, 23, 360, 'static');
+  wall1.addImage("stonewall.png");
+}
+
+function checkCollide(){
+  if (ball.colliding(wall1)) {
+    ball.color = "red";
+  }
+  else {
+    ball.color = "blue";
+  }
 }
 
 function createUFO(x, y) {
